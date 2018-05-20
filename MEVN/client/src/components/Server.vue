87b173d735a346fcbf05d5a613015710fc342964
Ignacio <royovillanova.i@gmail.com>
2018-05-20 19:07:49 +0200

    vista desde client ok

---
+++ MEVN/client/src/components/Server.vue
@@ -0,0 +1,28 @@
+<template>
+    <pre>{{ server }}</pre>
+</template>
+<script>
+import PostsService from '@/services/ApiServer'
+
+export default {
+  name: 'server',
+  data(){
+      return{
+          server:[]
+      }
+  },
+  mounted () {
+    this.getServer()
+  },
+    methods: {
+    async getServer () {
+      const response = await PostsService.fetchPosts()
+      this.server = response.data.posts
+    },
+  }
+}
+
+</script>
+<style>
+
+</style>
--- expressUno/routes/index.js
+++ expressUno/routes/index.js
@@ -5,5 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/about', function(req, res, next) {
+  res.send('Hola desde About');
+});
 
 module.exports = router;
